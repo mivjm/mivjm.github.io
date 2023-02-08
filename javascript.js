50d51093a90dfea54d81eebeadf63bfe75edf016
mivjm <53676220+mivjm@users.noreply.github.com>
2023-02-08 18:06:23 +0900

    rewrite cords under 'get click'

--- javascript.js
+++ javascript.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const overlayInner = document.getElementById('overlay-inner');
   overlayInner.addEventListener('click', stopEvent, false);  
 }, false);
+
 /*
 document.addEventListner('DOMContentLoaded', function(){
     // オーバレイを解放する関数
@@ -39,64 +40,55 @@ document.addEventListner('DOMContentLoaded', function(){
 */
 
 /*
-//　クリック位置取得（絶対）
-document.body.addEventListener( "click", function( event ) {
-	var x = event.pageX ;
-	var y = event.pageY ;
-} ) ;
-document.body.addEventListener( "click", function( event ) {
-  var x = event.pageX ;
-  var y = event.pageY ;
-} ) ;
+// クリック位置取得(絶対)
+document.body.addEventListener('click', function(event){
+	var x = event.pageX;
+	var y = event.pageY;
+}, false);
 
-//　クリック位置取得（相対）
-// <div id="target">...</div>などの要素にクリックイベントを設定
-document.getElementById( "target" ).addEventListener( "click", function( event ) {
-	var clickX = event.pageX ;
-	var clickY = event.pageY ;
-
+// クリック位置取得(相対)
+document.getElementById('target').addEventListener('click', function(event){
+	// 画面内におけるクリック位置を取得
+	var clickX = event.pageX;
+	var clickY = event.pageY;
+	
 	// 要素の位置を取得
-	var clientRect = this.getBoundingClientRect() ;
-	var positionX = clientRect.left + window.pageXOffset ;
-	var positionY = clientRect.top + window.pageYOffset ;
-
+	var clientRect = this.getBoundingClilentRect();
+	var positionX = clientRect.left + window.pageXOffset;
+	var positionY = clientRect.top + window.pageYOffset;
+	
 	// 要素内におけるクリック位置を計算
-	var x = clickX - positionX ;
-	var y = clickY - positionY ;
-} ) ;
+	var x = clickX - positionX;
+	var y = clickY - positionY;
+}, false);
 
 // HTML要素へ文字表示
-var sampleArea = document.getElementById("sampleArea");
-sampleArea.innerHTML = "表示したい文字列";
+var sampleArea = document.getElementById('sampleArea');
+sampleArea.innerHTML = '表示したい文字列';
 
 // ブラウザコンソールへ文字表示
-console.log("表示したい文字列");
-
+console.log('行事したい文字列');
 let price = 200;
 console.log('値段は' + price + '円です。');
->> 値段は200円です。
-
-let data = ['Orange', 'Melon'];
+let data = ['オレンジ', 'メロン'];
 console.log('商品は' + data + 'です。');
->> 商品はOrange,Melonです。
 */
 
 // 要素内のクリック位置をブラウザコンソールに表示
-/* document.getElementById( "clickTarget" ).addEventListener( "click", function( event ) { */
 document.getElementById('target').addEventListener('click', function(event){
-  // 絶対的なクリック位置を取得
-  var clickX = event.pageX;
-  var clickY = event.pageY;
-  
-  // 要素の位置を取得
-  var clientRect = this.getBoundingClientRect();
-  var positionX = clientRect.left + window.pageXOffset;
-  var positionY = clientRect.top + window.pageYOffset;
-  
-  // 要素内のクリック位置を計算
-  var x = clickX - positionX;
-  var y = clickY - positionY;
-  
-  // ブラウザコンソールへ結果を表示
-  console.log(' *** x:' + x + ' y:' + y);
+	// 絶対的なクリック位置を取得
+	var clickX = event.pageX;
+	var clickY = event.pageY;
+	
+	// 要素の位置を取得
+	var clientRect = this.getBoundingClientRect();
+	var positionX = clientRect.left + window.pageXOffset;
+	var positionY = clientRect.top + window.pageYOffset;
+	
+	// 要素内のクリック位置を計算
+	var x = clickX - positionX;
+	var y = clickY - positionY;
+	
+	// ブラウザコンソールへ結果を表示
+	console.log(' *** x:' + x + ' y:' + y);
 }, false);
