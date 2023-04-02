2264206bd58b5a4fa8234423aa24a3cb7756aea9
Masashi Motokado a.k.a mivjm a.k.a Masakado Murasaki <53676220+mivjm@users.noreply.github.com>
2023-04-02 20:20:56 +0900

    Add stars w/ canvas

--- javascript.js
+++ javascript.js
@@ -18,77 +18,42 @@ document.addEventListener('DOMContentLoaded', function(){
   overlayInner.addEventListener('click', stopEvent, false);  
 }, false);
 
-/*
-document.addEventListner('DOMContentLoaded', function(){
-    // オーバレイを解放する関数
-    const overlay = document.getElementById('overlay');
-    function overlayToggle() {
-      overlay.classList.toggle('overlay-on');
-    }
-    // 指定した要素に対して上記関数を実行するクリックイベントを設定
-    const clickArea = document.getElementsByClassName('overlay-event');
-    for (let i = 0; i < clickArea.length; i++) {
-      clickArea[i].addEventListener('click', overlayToggle, false);
-    }
-    // イベントに対してバブリングを停止
-    function stopEvent(event) {
-      event.stopPropagation();
-    }
-    const overlayInner = document.getElementById('overlay-inner');
-    overlayInner.addEventListener('click', stopEvent, false);
-}, false);
-*/
+// Stars w/ canvas
+// Set up canvas
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// Random helper
+function random(min, max) {
+  return min + Math.random() * (max + 1 - min);
+}
+// Reset stars by resize
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  //Re-add the stars
+  stars();
+});
+// Add stars
+function stars() {
+  //Add stars to a small fraction of the canvas
+  const canvasSize = canvas.width * canvas.height;
+  const starsFraction = canvasSize / 2000;
 
-/*
-// クリック位置取得(絶対)
-document.body.addEventListener('click', function(event){
-	var x = event.pageX;
-	var y = event.pageY;
-}, false);
+  for(let i = 0; i < starsFraction; i++) {
+    //Set up random elements
+    let xPos = random(2, canvas.width - 2);
+    let yPos = random(2, canvas.height - 2);
+    let alpha = random(0.5, 1);
+    let size = random(1, 2);
 
-// クリック位置取得(相対)
-document.getElementById('target').addEventListener('click', function(event){
-	// 画面内におけるクリック位置を取得
-	var clickX = event.pageX;
-	var clickY = event.pageY;
-	
-	// 要素の位置を取得
-	var clientRect = this.getBoundingClilentRect();
-	var positionX = clientRect.left + window.pageXOffset;
-	var positionY = clientRect.top + window.pageYOffset;
-	
-	// 要素内におけるクリック位置を計算
-	var x = clickX - positionX;
-	var y = clickY - positionY;
-}, false);
+    //Add stars
+    ctx.fillStyle = '#ffffff';
+    ctx.globalAlpha = alpha;
+    ctx.fillRect(xPos, yPos, size, size);
+  }
+}
 
-// HTML要素へ文字表示
-var sampleArea = document.getElementById('sampleArea');
-sampleArea.innerHTML = '表示したい文字列';
-
-// ブラウザコンソールへ文字表示
-console.log('行事したい文字列');
-let price = 200;
-console.log('値段は' + price + '円です。');
-let data = ['オレンジ', 'メロン'];
-console.log('商品は' + data + 'です。');
-
-// 要素内のクリック位置をブラウザコンソールに表示
-document.getElementById('target').addEventListener('click', function(event){
-	// 絶対的なクリック位置を取得
-	var clickX = event.pageX;
-	var clickY = event.pageY;
-	
-	// 要素の位置を取得
-	var clientRect = this.getBoundingClientRect();
-	var positionX = clientRect.left + window.pageXOffset;
-	var positionY = clientRect.top + window.pageYOffset;
-	
-	// 要素内のクリック位置を計算
-	var x = clickX - positionX;
-	var y = clickY - positionY;
-	
-	// ブラウザコンソールへ結果を表示
-	console.log(' *** x:' + x + ' y:' + y);
-}, false);
-*/
+//Add the stars
+stars();
