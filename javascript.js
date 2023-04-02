3c38cb7c5a8e8e3f5a63a17cea518d98e5ba866b
Masashi Motokado a.k.a mivjm a.k.a Masakado Murasaki <53676220+mivjm@users.noreply.github.com>
2023-04-02 20:33:47 +0900

    Add stars w/ Javascript

--- javascript.js
+++ javascript.js
@@ -18,42 +18,31 @@ document.addEventListener('DOMContentLoaded', function(){
   overlayInner.addEventListener('click', stopEvent, false);  
 }, false);
 
-// Stars w/ canvas
-// Set up canvas
-const canvas = document.querySelector('.canvas');
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-// Random helper
+// Stars w/ Javascript
 function random(min, max) {
   return min + Math.random() * (max + 1 - min);
 }
-// Reset stars by resize
-window.addEventListener('resize', function() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  //Re-add the stars
-  stars();
-});
-// Add stars
-function stars() {
-  //Add stars to a small fraction of the canvas
-  const canvasSize = canvas.width * canvas.height;
-  const starsFraction = canvasSize / 2000;
 
-  for(let i = 0; i < starsFraction; i++) {
-    //Set up random elements
-    let xPos = random(2, canvas.width - 2);
-    let yPos = random(2, canvas.height - 2);
-    let alpha = random(0.5, 1);
-    let size = random(1, 2);
+const body = document.querySelector('body');
+const canvasSize = body.offsetWidth * body.offsetHeight;
+const starsFraction = canvasSize / 2000;
 
-    //Add stars
-    ctx.fillStyle = '#ffffff';
-    ctx.globalAlpha = alpha;
-    ctx.fillRect(xPos, yPos, size, size);
-  }
+for(let i = 0; i < starsFraction; i++) {
+  // Set up random elements
+  let xPos = random(0, 100);
+  let yPos = random(0, 100);
+  let alpha = random(0.5, 1);
+  let size = random(1, 2);
+  let colour = '#ffffff';
+    
+  // Add them to the body
+  const star = document.createElement('div');
+  star.style.position = 'relative';
+  star.style.left = xPos + '%';
+  star.style.top = yPos + '%';
+  star.style.opacity = alpha;
+  star.style.width = size + 'px';
+  star.style.height = size + 'px';
+  star.style.backgroundColor = colour;
+  document.body.appendChild(star);
 }
-
-//Add the stars
-stars();
