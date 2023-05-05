e5b5e4f8b50fc45e2590e889c4af727fc770cee0
Masashi Motokado a.k.a mivjm a.k.a Masakado Murasaki <53676220+mivjm@users.noreply.github.com>
2023-05-05 11:05:37 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -319,8 +319,8 @@ function moonCalender() {
     // 12
     [18.1, 19.1, 20.1, 21.1, 22.1, 23.1, 24.1, 25.1, 26.1, 27.1, 28.1, 29.1, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5]
   ];
-  let m = mo -1;
-  var moonAge = moonAgeList[m][d];
+  let d = d -1;
+  var moonAge = moonAgeList[mo][d];
   alert(moonAge);
   return moonAge;
 }
